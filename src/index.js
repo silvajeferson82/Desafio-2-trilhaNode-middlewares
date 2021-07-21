944e39b25d6fa036de366e095a11c9c332a72c0a
silvajeferson82 <jefersonsilva.contato@gmail.com>
2021-07-20 23:57:46 -0300

    Desafio concluído

--- src/index.js
+++ src/index.js
@@ -10,20 +10,71 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if(!user){
+    return response.status(404).json({ error: 'User not found' });
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
-}
+  const { user } = request;
+  
+  if(user.pro === false && user.todos.length < 9){
+    return next();
+  }else if(user.pro === true){
+    return next();
+  }else{
+    return response.status(403).json({ error: 'Error message...'});
+  };
+};
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
-}
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find(user => user.username === username);
+  const idIsUuid = validate(id);
+
+  if(!user){
+    return response.status(404).json({error: 'Error message...'});
+  }
+
+  if(idIsUuid === false){
+    return response.status(400).json({error: 'Error message...'});
+  }
+
+  const todo = user.todos.find(todo => todo.id === id);
+
+  if(!todo){
+    return response.status(404).json({error: 'Error message...'});
+  }
+
+  request.todo = todo;
+  request.user = user;
+
+  return next();
+};
 
 function findUserById(request, response, next) {
-  // Complete aqui
-}
+  const { id } = request.params;
+
+  const user = users.find(user => user.id === id);
+
+  if(!user){
+    return response.status(404).json({error: 'Error message'})
+  };
+
+  request.user = user;
+
+  return next();
+};
 
 app.post('/users', (request, response) => {
   const { name, username } = request.body;
